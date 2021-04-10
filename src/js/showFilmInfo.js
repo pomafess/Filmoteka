ce9339d2cca7dee8daf12eeb645a022feeff1385
Maksym Sharpan <maksim.sharpan@gmail.com>
2021-04-10 19:13:07 +0300

    modalIsOpen

--- src/js/showFilmInfo.js
+++ src/js/showFilmInfo.js
@@ -1,19 +1,24 @@
 import MovieHttpService from './MovieHttpService.js';
-import filmInfoTemplate from '../templates/film-info.hbs';
 import openModal from './openModal.js';
+
+import filmInfoTemplate from '../templates/film-info.hbs';
+
 const movieHttpService = new MovieHttpService();
 
 function showFilmInfo(e) {
-    e.preventDefault();
-    const { target } = e;
-    if (target.classList.contains('')) {
-        const { filmId } = target.dataset;
+  e.preventDefault();
 
-        //data = результат вызова movieHttpService для передачи в шаблон.
-        const filmInfo = filmInfoTemplate(data);
-        openModal(filmInfo);
+  const { target } = e;
 
-    }
-    
+  if (target !== this) {
+    const { id } = target.dataset;
+    const data = movieHttpService.getMovieInfo(id);
+    data.then(result => {
+      //data = результат вызова movieHttpService для передачи в шаблон.
+      const filmInfo = filmInfoTemplate(result);
+      console.log(filmInfo);
+      openModal(filmInfo);
+    });
+  }
 }
 export default showFilmInfo;
