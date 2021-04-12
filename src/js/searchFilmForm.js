8c1e35861dab3bfdb2957cafb758ba4c511ea86a
Yaroslav Hliy <hliy.yaroslav@gmail.com>
2021-04-12 09:48:14 +0300

    input err

--- src/js/searchFilmForm.js
+++ src/js/searchFilmForm.js
@@ -9,23 +9,26 @@ async function searchFilmForm(e) {
     const listGallery = document.querySelector('.film-list');
     const myInput = this.querySelector("[name=query]");
     const myInputValue = myInput.value;
+    const errorMsg = this.querySelector('.not-found');
     
     try {
         filmsSearchOptions.options.query = myInputValue;
         filmsSearchOptions.endpoint = "search/movie";
         const films = await movieHttpService.get(filmsSearchOptions);        
-        if (!films.results.length) {
+        if ( !films.results.length) {
             myInput.insertAdjacentHTML("afterend", `
             <p class="not-found">Search result not successful. Enter the correct movie name</p>
             `)
         } else {
             renderFilms(films, listGallery);
-            const errorMsg = this.querySelector('.not-found');
             if (errorMsg) {
                 errorMsg.remove();
             }
         }
         this.reset();
+        if (errorMsg) {
+            errorMsg.remove();
+        }
     }
     catch (error) {
         console.log(error)
