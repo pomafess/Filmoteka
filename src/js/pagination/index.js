62336685114290e29f4da7d0c885697abcc5bece
Roman Bilousov <pomafess@gmail.com>
2021-04-12 11:53:42 +0300

    add pagination

--- src/js/pagination/index.js
+++ src/js/pagination/index.js
@@ -2,8 +2,8 @@ import paginationTemplate from '../../templates/pagination.hbs';
 import MovieHttpService from '../MovieHttpService.js';
 import createPaginationArray from "./createPaginationArray.js";
 import filmsSearchOptions from "../filmsSearchOptions.js";
-import onPaginationBtnClick from './onPaginationBtnClick.js';
 import renderFilms from "../renderMainPage";
+import svg from '../../images/sprite.svg';
 
 const movieHttpService = new MovieHttpService();
 
@@ -11,25 +11,57 @@ const createPagination = (total_pages, filmsContainer) => {
     const { page } = filmsSearchOptions.options;
     const arr = createPaginationArray(total_pages);
 
+    const prevPagination = document.querySelector(".pagination-container");
+    if (prevPagination){
+        prevPagination.remove()
+    }
+
     const paginationContainer = document.createElement("div");
     paginationContainer.className = "pagination-container";
 
-    paginationContainer.innerHTML = paginationTemplate({ src: "", list: arr, page });
-    const numbers = paginationContainer.querySelectorAll(".pages-list-link");
-    const activeElement = [].find.call(numbers, item => item.textContent === `${page}`);
-    activeElement.closest("li").classList.add("current-item");
-    
+    paginationContainer.innerHTML = paginationTemplate({ src: svg, list: arr, page });
+    const links = paginationContainer.querySelectorAll('.pages-list-link');
+    const dots = [].filter.call(links, item => item.textContent === '...');
+    dots.forEach(dot => {
+        dot.removeAttribute("data-id");
+        dot.closest("li").removeAttribute("data-id");
+    });
 
-    paginationContainer.addEventListener('click', async (e) => {
+    const activeElement = [].find.call(links, item => item.textContent === `${page}`);
+    activeElement.closest("li").classList.add("current-item");
+
+    const arrowButtons = paginationContainer.querySelectorAll('.arrow-btn');
+    if (page > 1) {
+        
+        arrowButtons[0].classList.add('active')
+    }
+    if (page < total_pages) {
+        arrowButtons[1].classList.add('active')
+    }
+
+    paginationContainer.addEventListener("click", async function (e) {
         e.preventDefault();
-        filmsSearchOptions.options.page = +e.target.textContent;
-        const { endpoint, options } = filmsSearchOptions;
-        const filmsData = await movieHttpService.get({ endpoint, options });
-        paginationContainer.remove()
+        const { id } = e.target.dataset;
+        if (!id) {
+            return;
+        }
+        switch (id) {
+            case "prev":
+                filmsSearchOptions.options.page--;
+                break;
+            case "next":
+                filmsSearchOptions.options.page++;
+                break;
+            default:
+                const number = +e.target.textContent.trim();
+                filmsSearchOptions.options.page = number;
+        }
+
+        const filmsData = await movieHttpService.get(filmsSearchOptions);
         renderFilms(filmsData, filmsContainer);
     });
+    
     return paginationContainer;
-
 }
 
 export default createPagination;
